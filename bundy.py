08ba4a77c26b80c8c896fb744db8043d6fad5fd1
Ali Ahsan <ali.ahsan@ordergroove.com>
2024-01-05 00:44:02 -0500

    Adding streamlit app

---
+++ bundy.py
@@ -0,0 +1,122 @@
+import requests
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import tiktoken
+import openai
+import numpy as np
+from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
+from ast import literal_eval
+import time
+import streamlit as st
+
+# Regex pattern to match a URL
+HTTP_URL_PATTERN = r'^http[s]{0,1}://.+$'
+
+# Define OpenAI api_key
+openai.api_key = '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'
+
+################################################################################
+### READ the Embeddings file CSV into a Pandas Data Frame
+################################################################################
+
+df=pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(literal_eval).apply(np.array)
+
+df.head()
+
+################################################################################
+### For the given question and the given pandas dataframe find create the relevant context
+################################################################################
+
+def create_context(
+    question, df, max_len=1800, size="ada"
+):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
+
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+        
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+        
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+        
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return "\n\n###\n\n".join(returns)
+
+def answer_question(
+    df,
+    model="text-davinci-003",
+    question="Am I allowed to publish model outputs to Twitter, without a human review?",
+    max_len=1800,
+    size="ada",
+    debug=False,
+    max_tokens=150,
+    stop_sequence=None
+):
+    """
+    Answer a question based on the most similar context from the dataframe texts
+    """
+    context = create_context(
+        question,
+        df,
+        max_len=max_len,
+        size=size,
+    )
+    # If debug, print the raw model response
+    if debug:
+        print("Context:\n" + context)
+        print("\n\n")
+
+    try:
+        # Create a completions using the question and context
+        response = openai.Completion.create(
+            prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+            temperature=0,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=stop_sequence,
+            model=model,
+        )
+        return response["choices"][0]["text"].strip()
+    except Exception as e:
+        print(e)
+        return ""
+
+################################################################################
+### Get Question and try to answer it with the context
+################################################################################
+
+st.title("I am your assistant trained on Nich Bundy's knoweldge, who was trained by Andrew Magnee, ask me any question that you think the Knowldege Center articles should be able to answer")
+
+question_input = st.text_input("Question:")
+
+if question_input:
+	 answer = answer_question(df, question=question_input)
+	 st.text_area("Answer:", answer)
+
+#print(answer_question(df, question="What day is it?", debug=True))
+
+#print(answer_question(df, question="What is our newest embeddings model?"))
